eec9023aad275ba7e6a22273667ae55cbf27ee99
Ghislain Vaillant <ghislain.vaillant@icm-institute.org>
2023-09-07 18:18:31 +0200

    Revamp build recipe

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env bash
 
-mkdir build
-cd build
+set -ex
 
-export CFLAGS="${CFLAGS} -I ${PREFIX}/include/eigen3"
 export CXXFLAGS="${CXXFLAGS} -I ${PREFIX}/include/eigen3"
 
-cmake $CMAKE_ARGS -GNinja \
-    -DCMAKE_BUILD_TYPE:STRING=Release \
-    -DCMAKE_INSTALL_PREFIX:STRING=$PREFIX \
-    -DCONVERT3D_USE_ITK_REMOTE_MODULES:BOOL=OFF \
-    ..
+# config
+cmake ${CMAKE_ARGS} \
+	-D CMAKE_BUILD_TYPE:STRING=Release \
+    	-D CMAKE_INSTALL_PREFIX:STRING=${PREFIX} \
+    	-D CONVERT3D_USE_ITK_REMOTE_MODULES:BOOL=OFF \
+    	-G Ninja \
+	-B build \
+	-S ${SRC_DIR}
 
-cmake --build .
+# build
+cmake --build build --parallel ${CPU_COUNT}
 
-ctest --extra-verbose --output-on-failure .
+# install
+cmake --install build
 
-cmake --install .
+# test
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
+ctest --test-dir build --extra-verbose --output-on-failure
+fi
